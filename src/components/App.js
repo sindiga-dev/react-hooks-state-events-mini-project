837f9610201c58dd62e131371bbdcedfe827eb27
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-28 15:13:39 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-import { useState } from "react";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+
+/*console.log("Here's the data you're working with");
+console.log({ CATEGORIES, TASKS });*/
 
 function App() {
-	const [tasks, setTasks] = useState(TASKS);
-	function deleteTasks() {
-		let newTaskslist = tasks.filter((task) => task.text !== task);
-		setTasks(newTaskslist);
-	}
-	return (
-		<div className="App">
-			<h2>My tasks</h2>
-			<CategoryFilter />
-			<NewTaskForm />
-			<TaskList tasks={tasks} deleteTasks={deleteTasks} />
-		</div>
-	);
+
+  const [task, setTask] = useState(TASKS)
+  const [categories, setCategories] = useState(CATEGORIES)
+  const [selectedCategoryButton, setSelectedCategoryButton] = useState('All')
+  
+  
+
+  function addNewItemtoList(newItem){
+    setTask([...task,newItem])
+  }
+
+  function deletedItem(deletedItem){
+    setTask(task.filter((item)=>item.text !== deletedItem))
+  }
+ 
+
+  const itemDisplayed = task
+
+  .filter(
+    (item)=>{ 
+    if(selectedCategoryButton==='All') return true
+    return selectedCategoryButton === item.category
+   } 
+   )
+
+  return (
+    <div className="App">
+      <h2>My tasks</h2>
+      <CategoryFilter categories={categories}
+        onButton={selectedCategoryButton}
+        selectedButton={setSelectedCategoryButton}  />
+      <NewTaskForm onTaskFormSubmit={addNewItemtoList}
+        categories={categories} />
+      <TaskList deletedItem={deletedItem}
+        tasks={itemDisplayed} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Task from "./Task";
 
-function TaskList({ tasks, deleteTasks }) {
+function TaskList({tasks,deletedItem}) {
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
-      {tasks.map((task) => (
-        <Task key={task.text} text={task.text} category={task.category} deleteTasks={deleteTasks} />
+
+      {tasks.map((task,index)=>(
+        <Task 
+          deletedItem={deletedItem}
+          key={index}
+          text={task.text}
+          category={task.category}
+         />
       ))}
     </div>
   );
